a2bb3b65f61e1a61d17d8d517fd4dd09f862bacb
Ylli <yll.gashi29@gmail.com>
2024-05-09 18:50:39 +0200

    added more info

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,41 @@
-import { TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {Tab} from "./interface/tab";
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
+
+  describe('tabList', () => {
+    // arrange
+    let tabList: Tab[];
+    beforeEach(() => {
+      // arrange
+      tabList = component.tabList;
+    })
+    it('should be defined', () => {
+
+      // assert
+      expect(tabList).toBeDefined();
+    })
+
+    it('should have 3 elements', () => {
+
+      // assert
+      expect(tabList.length).toEqual(3);
+    })
+  })
 
 });
